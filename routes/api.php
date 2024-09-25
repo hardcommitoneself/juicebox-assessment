d676f967718a2af08f0e0204f960056fdbc106f8
hardcommitoneself <hardcommitoneself@gmail.com>
2024-09-24 19:34:06 -0700

    done: implemented login api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Post\PostAllController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+/**
+ * API endpoints for auth
+ */
+Route::post('/login', LoginController::class);
 
 Route::middleware(['auth:sanctum'])->group(function() {
     /**
      * API endpoints for Post
      */
-    Route::get('posts', PostAllController::class);
+    Route::get('/posts', PostAllController::class);
 });
