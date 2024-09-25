81a5f5737c01be7106e45c010f81a0d28e99d089
hardcommitoneself <hardcommitoneself@gmail.com>
2024-09-24 19:52:58 -0700

    done: implemented logout api

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        try {
+            Auth::guard('sanctum')->user()->tokens()->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged Out Successfully',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Post\PostAllController;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,7 @@ Route::post('/login', LoginController::class);
 Route::post('/register', RegisterController::class);
 
 Route::middleware(['auth:sanctum'])->group(function() {
+    Route::post('/logout', LogoutController::class);
     /**
      * API endpoints for Post
      */
